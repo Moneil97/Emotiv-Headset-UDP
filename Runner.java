e7d838bbbfc855d6096324cd20ef4f8d4b74d57f
Michael O'Neil <cam8059@yahoo.com>
2018-07-09 16:40:19 -0400

    Added Error Checking

--- Runner.java
+++ Runner.java
@@ -4,9 +4,20 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import javax.swing.JOptionPane;
+
 public class Runner {
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		String libs = new File(Runner.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent() + "\\EmotivLibs";
-		Runtime.getRuntime().exec("java -Djava.library.path=" + libs + " -jar test.jar");
+		
+		if (new File(libs).exists()) {
+			if (new File(libs + "\\edk.dll").exists())
+				Runtime.getRuntime().exec("java -Djava.library.path=" + libs + " -jar EEGImporter.jar");
+			else
+				JOptionPane.showMessageDialog(null, "Could not find file in EmotivLibs: edk.dll");
+		}
+		else
+			JOptionPane.showMessageDialog(null, "Could not find folder: EmotivLibs, please make sure it is in the same folder as this jar file");
+		
 	}
 }
