1cc4c5de0cb0bbcac8b67aa747cdfc0079ec5da9
Michael O'Neil <cam8059@yahoo.com>
2018-07-10 16:14:42 -0400

    Create EEGimporterUDPClock.m

---
+++ EEGimporterUDPClock.m
@@ -0,0 +1,50 @@
+classdef EEGimporterUDP < matlab.System
+    % Untitled Add summary here
+    %
+    % This template includes the minimum set of functions required
+    % to define a System object with discrete state.
+    
+    % Public, tunable properties
+    properties
+        
+    end
+    
+    properties (Hidden)%(DiscreteState)
+        
+    end
+    
+    % Pre-computed constants
+    properties(Access = private)
+        
+    end
+    
+    methods(Access = protected)
+        %         function setupImpl(obj)
+        %             % Perform one-time calculations, such as computing constants
+        %             obj.hudpr = dsp.UDPReceiver('LocalIPPort',9091,'RemoteIPAddress','127.0.0.1','ReceiveBufferSize',1024);
+        %         end
+        
+        function y = stepImpl(obj, u)
+            % Implement algorithm. Calculate y as a function of input u and
+            % discrete states.
+            coder.extrinsic('typecast');
+            coder.extrinsic('swapbytes');
+            coder.extrinsic('evalin');
+            coder.extrinsic('step');
+            hudpr = evalin('base', 'hudpr');
+            
+            dataReceived = step(hudpr);
+            y = zeros(20,1);
+            
+            while (isempty(dataReceived))
+                dataReceived = step(hudpr);
+            end
+            
+            y = swapbytes(typecast((dataReceived), 'double'));
+        end
+        
+        function resetImpl(obj)
+            % Initialize / reset discrete-state properties
+        end
+    end
+end
